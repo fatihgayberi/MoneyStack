7e78d35f575e10e96d02bd8e6276c061d9990386
Fatih Gayberi <46620361+fatihgayberi@users.noreply.github.com>
2020-10-08 22:49:00 +0300

    Controller
    Player control added.

--- MoneyStack/Assets/Script/MoneyController.cs
+++ MoneyStack/Assets/Script/MoneyController.cs
@@ -4,15 +4,84 @@ using UnityEngine;
 
 public class MoneyController : MonoBehaviour
 {
+    Rigidbody banknoteRB;
+    public float banknoteSpeed;
+    public float laneChangeSpeed;
+    float firstX;
+    float endX;
+    float currentX;
+    bool change;
+    Vector3 targetPath;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        banknoteRB = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        BanknoteController();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        BanknoteMove();
+        LaneChange();
+    }
+
+    void BanknoteMove()
+    {
+        banknoteRB.velocity = new Vector3(0, 0, banknoteSpeed);
+    }
+
+    void BanknoteController()
+    {
+        if (Input.GetMouseButtonDown(0) && !change)
+        {
+            firstX = Input.mousePosition.x;
+        }
+        if (Input.GetMouseButtonUp(0) && !change)
+        {
+            endX = Input.mousePosition.x;
+
+            if (endX - firstX > 0)
+            {
+                // RIGHT
+                currentX = transform.position.x + 5f;
+
+                if (currentX <= 5)
+                {
+                    change = true;
+                }
+            }
+            if (endX - firstX < 0)
+            {
+                // LEFT
+                currentX = transform.position.x - 5f;
+
+                if (currentX >= -5)
+                {
+                    Debug.Log("asdf");
+                    change = true;
+                }
+            }
+        }
+    }
+
+    void LaneChange()
+    {
+        if (change)
+        {
+            targetPath = new Vector3(currentX, transform.position.y, transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, targetPath, Time.deltaTime * laneChangeSpeed);
+
+            if (transform.position.x >= 5f || transform.position.x == 0 || transform.position.x <= -5f)
+            {
+                transform.position = targetPath;
+                change = false;
+            }
+        }
     }
 }
